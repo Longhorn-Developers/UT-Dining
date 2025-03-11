1fb7ae7d2c671dcae5141fbe6b1565105ec14aee
David Nguyen <dngames2006@gmail.com>
2025-03-11 17:29:18 -0500

    feat: add clear search query X button in TextInput (#39)
    * feat: added clear search query x button in textinput
    
    * feat: used color instead of hard-coded hex color values

--- app/location/components/SearchBar.tsx
+++ app/location/components/SearchBar.tsx
@@ -1,6 +1,8 @@
-import { Search } from 'lucide-react-native';
+import { Search, X } from 'lucide-react-native';
 import React from 'react';
 import { View, TextInput } from 'react-native';
+
+import { COLORS } from '~/utils/colors';
 
 type Props = {
   query: string;
@@ -10,14 +12,14 @@ type Props = {
 const SearchBar = ({ query, setQuery }: Props) => {
   return (
     <View className="flex-row items-center rounded-lg border border-ut-grey/15 bg-white px-3 py-2.5">
-      <Search size={18} color="#888888" />
+      <Search size={18} color={COLORS['ut-grey']} />
       <TextInput
         className="ml-2 min-h-4 flex-1 text-base leading-tight placeholder:text-ut-grey/75"
         placeholder="Search for food name..."
         value={query}
         onChangeText={setQuery}
-        clearButtonMode="while-editing"
       />
+      {query.length > 0 && <X size={18} color={COLORS['ut-grey']} onPress={() => setQuery('')} />}
     </View>
   );
 };
