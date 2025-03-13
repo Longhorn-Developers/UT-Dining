26db0e322a7203557f52c5503322b64169cba5c0
Ethan <ethanlanting@gmail.com>
2025-03-13 15:03:31 -0500

    feat: add favorites table and related functionality for managing favorite food items

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,6 +2,7 @@
 
 import m0000 from './0000_bouncy_black_knight.sql';
 import m0001 from './0001_vengeful_thunderbolt.sql';
+import m0002 from './0002_grey_sharon_carter.sql';
 import journal from './meta/_journal.json';
 
 export default {
@@ -9,5 +10,6 @@ export default {
   migrations: {
     m0000,
     m0001,
+    m0002,
   },
 };
