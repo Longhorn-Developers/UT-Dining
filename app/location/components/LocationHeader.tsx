6313a650157e29a0d6cb1c3fa29e5d743ea85f32
Ethan <ethanlanting@gmail.com>
2025-03-16 20:03:50 -0500

    fix: enhance location status handling with menu checks

--- app/location/components/LocationHeader.tsx
+++ app/location/components/LocationHeader.tsx
@@ -1,5 +1,6 @@
+import { eq } from 'drizzle-orm';
 import * as Haptics from 'expo-haptics';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 import SearchBar from './SearchBar';
@@ -7,6 +8,8 @@ import TimeSchedule from './TimeSchedule';
 
 import FilterBar from '~/components/FilterBar';
 import TopBar from '~/components/TopBar';
+import { menu, location as location_schema } from '~/db/schema';
+import { useDatabase } from '~/hooks/useDatabase';
 import { generateSchedule, isLocationOpen } from '~/utils/time';
 
 interface LocationHeaderProps {
@@ -20,8 +23,38 @@ interface LocationHeaderProps {
 
 const LocationHeader = React.memo(
   ({ location, selectedMenu, setSelectedMenu, filters, query, setQuery }: LocationHeaderProps) => {
+    const [open, setOpen] = useState(false);
+    const db = useDatabase();
     const [timeDropdownOpen, setTimeDropdownOpen] = useState(false);
     const schedule = useMemo(() => generateSchedule(location), [location]);
+
+    useEffect(() => {
+      const checkOpen = async () => {
+        // Checking if there are any menus for the location
+        // Get location id
+        const locationData = db
+          .select()
+          .from(location_schema)
+          .where(eq(location_schema.name, location))
+          .get();
+
+        if (!locationData) {
+          setOpen(false);
+          return;
+        }
+
+        const res = db.select().from(menu).where(eq(menu.location_id, locationData.id)).get();
+
+        if (!res) {
+          setOpen(false);
+          return;
+        }
+
+        setOpen(isLocationOpen(location));
+      };
+
+      checkOpen();
+    }, [location]);
 
     return (
       <View className="mx-6 mt-6 flex gap-y-5">
