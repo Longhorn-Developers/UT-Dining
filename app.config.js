943d537872104af52970bd1183eaf0c5c313aa7a
Ethan <ethanlanting@gmail.com>
2025-05-27 14:36:03 -0500

    chore: update version to 1.1.0 in config files

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? 'UT Dining (Dev)' : 'UT Dining',
     slug: 'ut-dining',
-    version: '1.0.0',
+    version: '1.1.0',
     scheme: 'ut-dining',
     web: {
       bundler: 'metro',
