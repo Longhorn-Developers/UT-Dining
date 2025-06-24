f712b58e93a00b30327eb3e2a4602c84a3b1ac0c
Ethan <ethanlanting@gmail.com>
2025-06-24 16:09:40 -0500

    refactor: update Supabase URL to use environment variable for better configuration management

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from '~/types/database.types';
 
-const supabaseUrl = 'https://1bcd-24-242-137-154.ngrok-free.app';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl as string, supabaseAnonKey as string);
