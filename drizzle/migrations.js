d2cfc65943bc3a014ac8dae442a51bd8df1184bc
Ethan <ethanlanting@gmail.com>
2025-07-01 23:40:29 -0500

    chore: run lint

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,6 +1,5 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
-import journal from './meta/_journal.json';
 import m0000 from './0000_bouncy_black_knight.sql';
 import m0001 from './0001_vengeful_thunderbolt.sql';
 import m0002 from './0002_grey_sharon_carter.sql';
@@ -12,21 +11,21 @@ import m0007 from './0007_noisy_alice.sql';
 import m0008 from './0008_nosy_alex_power.sql';
 import m0009 from './0009_wide_red_skull.sql';
 import m0010 from './0010_normal_purifiers.sql';
+import journal from './meta/_journal.json';
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005,
-m0006,
-m0007,
-m0008,
-m0009,
-m0010
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+    m0001,
+    m0002,
+    m0003,
+    m0004,
+    m0005,
+    m0006,
+    m0007,
+    m0008,
+    m0009,
+    m0010,
+  },
+};
